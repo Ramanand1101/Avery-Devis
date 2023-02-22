ec698803525d3e56f390a5adfe80ac418fdb394d
Ramanand Tiwari <112823601+Ramanand1101@users.noreply.github.com>
2023-02-23 01:15:09 +0530

    landing page all done except footer

---
+++ Frontend/index.js
@@ -0,0 +1,21 @@
+const sliderWrapper = document.querySelector('.slider-wrapper');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let slideIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = sliderWrapper.children.length - 3; /* adjust this value to change the number of images displayed */
+  }
+  sliderWrapper.style.transform = `translateX(-${slideIndex * (100 / 3)}%)`; /* adjust this value to change the number of images displayed */
+});
+
+nextBtn.addEventListener('click', () => {
+  slideIndex++;
+  if (slideIndex >= sliderWrapper.children.length - 2) { /* adjust this value to change the number of images displayed */
+    slideIndex = 0;
+  }
+  sliderWrapper.style.transform = `translateX(-${slideIndex * (100 / 3)}%)`; /* adjust this value to change the number of images displayed */
+});
